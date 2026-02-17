866ba35c4a802fdbb77ce9b2e4dd242a1563c39c
Abu <abubakarbouy246@gmail.com>
2026-02-17 11:56:08 +0000

    psi values and cold bridges placeholder api code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from src.image import Image
 from aggregate_calculations import get_psis
 
 class ImagePayload(BaseModel):
@@ -12,7 +13,11 @@ class ImagePayload(BaseModel):
 
 app = FastAPI()
 
-@app.get("/psis")
+@app.get("/analyse-image")
 def psi_values(images : list[ImagePayload]):
+    images : list[Image] = process(images)  # placeholder function, expected to return Image instances with calculated fields
     psis = get_psis(images)  # returns the psi values as a numpy array
-    return {"psis": psis.tolist()}
+    cold_bridges = [i.cb_pix for i in images]
+    
+    return {"psis": psis.tolist(), "cold_bridges": cold_bridges}
+
