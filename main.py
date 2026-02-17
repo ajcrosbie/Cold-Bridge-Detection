d14c4a269a3f3d03dd401c4f4d35d27d22ed5929
Abu <abubakarbouy246@gmail.com>
2026-02-17 12:10:51 +0000

    Emissivity value added to image payload + refactored analyse-images

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ class ImagePayload(BaseModel):
     pixels: list[float]
     int_amb_temp: float
     ext_temp: float
+    emissivity:float
     pixel_length: float
     wall_height: float
 
 
 app = FastAPI()
 
-@app.get("/analyse-image")
-def psi_values(images : list[ImagePayload]):
-    images : list[Image] = process(images)  # placeholder function, expected to return Image instances with calculated fields
+@app.get("/analyse-images")
+def analyse_images(images : list[ImagePayload]):
+    images = [Image(i.pixels, None, None, i.int_amb_temp, i.ext_temp, i.emissivity, i.pixel_length, i.wall_length) for i in images]
+    images = process(images)  # placeholder function, expected to calculate and fill in the None fields
     psis = get_psis(images)  # returns the psi values as a numpy array
     cold_bridges = [i.cb_pix for i in images]
     
