43d86eb925cdc662663c59dbc9feed4db3e77c90
ajcrosbie <aaron.j.crosbie@gmail.com>
2026-03-09 18:10:17 +0000

    run_images updated

--- src/computer_vision/imageRunnner.py
+++ src/computer_vision/imageRunnner.py
@@ -2,6 +2,13 @@ from extract_cold_bridge  import *
 from extract_temps import *
 
 
-def run_images(img_path: PathLike) -> Box:
-    timg, tmin, tmax = image_to_temperature_map(img_path)
-    return find_bridge(detect_cold_mask(tmin, tmax, timg))
+
+def run_images(img_paths:list[PathLike]) -> list[tuple[np.ndarray, float]]:
+    l = []
+    for i in img_paths:
+        timg, tmin, tmax = image_to_temperature_map(i)
+        cb = find_bridge(detect_cold_mask(tmin, tmax, timg))
+        l.append(extract_from_box(timg, cb), find_mean(timg, cb))
+        
+    return l
+
